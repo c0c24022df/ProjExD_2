35cac64845ef07a89e1f5f6409f277eb81b5097e
c0c24022df <c0c24022df@edu.teu.ac.jp>
2025-06-17 14:46:20 +0900

    練習1,2,3,4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,10 +1,25 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {  #移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, 5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (5, 0),
+    }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True  #初期値：画面内
+    if rct.left < 0 or WIDTH < rct.right:  #横方向の画面外判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  #縦方向の画面外判定
+        tate = False
+    return yoko, tate  #横方向,縦方向の画面内判定結果を返す
 
 
 def main():
@@ -14,26 +29,50 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(225,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):  #こうかとんRectと爆弾Rectの衝突判定
+            print("Game Over")
+            return
+
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0] 
+                sum_mv[1] += mv[1]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)  #爆弾移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx*=-1  #横方向にはみ出ていたら
+        if not tate:
+            vy *=-1  #縦方向にはみ出ていたら
+        screen.blit(bb_img, bb_rct)  #爆弾描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
